7c3afb384c9f333234a0cd19edf993fb44420665
Dmitry Kiselev <kdem27@gmail.com>
2016-11-27 21:08:03 +0200

    create registration/login view
    This view provide ability to register for new users and
    log in for existing.
    It also provide error messages to template.

--- authorization/views.py
+++ authorization/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from .forms import RegistrationForm, LoginForm
+import json
+from django.contrib.auth import login
 
-# Create your views here.
+
+def auth(request):
+    login_form, registration_form = False, False
+    if request.method == "POST":
+        data = json.loads(request.body.decode("utf-8"))
+        if "email" in data: # some condition to distinguish between login and registration form
+            registration_form = RegistrationForm(data)
+            if registration_form.is_valid():
+                registration_form.save()
+                return JsonResponse({'response': 'Now you can log in'})
+            else:
+                return JsonResponse({'has_error': True, 'errors': registration_form.error_messages})
+        else:
+            login_form = LoginForm(data)
+            if login_form.is_valid():
+                login(request, login_form.get_user())
+                return JsonResponse({'response': 'You are logged in'})
+            else:
+                return JsonResponse({'has_error': True, 'errors': login_form.error_messages})
+
+    obj = {
+        'login_form': login_form if login_form else LoginForm(),
+        'registration_form': registration_form if registration_form else RegistrationForm(),
+    }
+    return render(request, 'authorization/authForm.html', obj)
