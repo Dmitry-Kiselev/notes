f20a74744546e43132a2692cbaafd8b35d641f4e
Dmitry Kiselev <kdem27@gmail.com>
2016-11-29 21:10:53 +0200

    login automatically after registration

--- authorization/views.py
+++ authorization/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from .forms import RegistrationForm, LoginForm
 import json
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 
 
 def auth(request):
     login_form, registration_form = False, False
     if request.method == "POST":
         data = json.loads(request.body.decode("utf-8"))
-        if data.get('email'): # some condition to distinguish between login and registration form
+        if data.get('email'):  # some condition to distinguish between login and registration form
             registration_form = RegistrationForm(data)
             if registration_form.is_valid():
                 registration_form.save()
+                new_user = authenticate(username=data['username'],
+                                        password=data['password1'],
+                                        )
+                login(request, new_user)
                 return JsonResponse({'response': 'Now you can log in'})
             else:
                 return JsonResponse({'has_error': True, 'errors': registration_form.error_messages})
