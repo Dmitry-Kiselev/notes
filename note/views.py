557aafede9751710b735c6ffdc5693e53e21d8b4
Dmitry Kiselev <kdem27@gmail.com>
2016-12-03 20:00:34 +0200

    add NoteDetails view
    this class provides methods that allows user to:
    
    - get details about specific note
    
    - update existing note
    
    - delete note

--- note/views.py
+++ note/views.py
@@ -2,6 +2,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+from django.http import Http404
+from django.core.exceptions import PermissionDenied
+
 from .models import Note
 from .serializers import NoteSerializer
 
@@ -25,3 +28,37 @@ class NoteList(APIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class NoteDetails(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            note = Note.objects.get(pk=pk)
+            if note.author == self.request.user:
+                return note
+            else:
+                return PermissionDenied()
+        except Note.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        note = self.get_object(pk)
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
