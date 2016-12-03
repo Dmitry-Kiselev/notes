539798d140760c9ba8fbc9e4d6f0b9e6096c9595
Dmitry Kiselev <kdem27@gmail.com>
2016-12-03 20:04:45 +0200

    associate note with the user

--- note/serializers.py
+++ note/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Note
 
 class NoteSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author')
 
     class Meta:
         model = Note
