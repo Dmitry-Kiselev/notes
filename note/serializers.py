a0f9d802352c0f8078a810a7b4a5345075fd217f
Dmitry Kiselev <kdem27@gmail.com>
2016-12-03 21:52:41 +0200

    fix: source parameter must refer to object's attribute

--- note/serializers.py
+++ note/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Note
 
 class NoteSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author')
+    author = serializers.ReadOnlyField(source='author.id')
 
     class Meta:
         model = Note
