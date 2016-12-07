8e3e932fa99c7c5bf378ba9cf69d2785d1e0f876
Dmitry Kiselev <kdem27@gmail.com>
2016-12-07 20:20:19 +0200

    use generic views

--- note/views.py
+++ note/views.py
@@ -1,70 +1,32 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework import permissions
-
-from django.http import Http404
-from django.core.exceptions import PermissionDenied
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Note
-from .serializers import NoteSerializer
 from .permissions import IsOwnerOrDenial
+from .serializers import NoteSerializer
 
 
-class NoteList(APIView):
+class NoteList(ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
 
+    serializer_class = NoteSerializer
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrDenial,)
 
-    def get(self, request):
-        notes = Note.objects.filter(pk=request.user.pk)
-        serializer = NoteSerializer(notes, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = NoteSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        return Note.objects.filter(pk=self.request.user.pk)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class NoteDetails(APIView):
+class NoteDetails(RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance.
     """
-
+    serializer_class = NoteSerializer
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrDenial,)
 
-    def get_object(self, pk):
-        try:
-            note = Note.objects.get(pk=pk)
-            if note.author == self.request.user:
-                return note
-            else:
-                return PermissionDenied()
-        except Note.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        note = self.get_object(pk)
-        note.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        return Note.objects.filter(pk=self.request.user.pk)
