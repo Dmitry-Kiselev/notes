d42b1e4d832a562f6c0d5e77c365482cb61ea5ae
Dmitry Kiselev <kdem27@gmail.com>
2016-12-07 20:23:27 +0200

    add serializers for labels and categories

--- note/serializers.py
+++ note/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import Note
+
+from .models import Note, Label, Category
+
 
 class NoteSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.id')
@@ -7,3 +9,19 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = '__all__'
+
+
+class LabelSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='author.id')
+
+    class Meta:
+        model = Label
+        fields = ('name',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='author.id')
+
+    class Meta:
+        model = Category
+        fields = ('name',)
