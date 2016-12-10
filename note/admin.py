46bb1f1214d81ee969ac9e453ddfe05bd60a308d
Dmitry Kiselev <kdem27@gmail.com>
2016-12-10 15:47:36 +0200

    register label and category models

--- note/admin.py
+++ note/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Label, Category
 
 
 # Register your models here.
 admin.site.register(Note)
+admin.site.register(Label)
+admin.site.register(Category)
