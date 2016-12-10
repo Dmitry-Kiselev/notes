832cbc82b8650af5d0e3c1e5ee42648c7565db01
Dmitry Kiselev <kdem27@gmail.com>
2016-12-10 14:24:07 +0200

    add ability to retrieve data

---
+++ note/static/js/services.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dmitry on 07/12/2016.
+ */
+'use strict';
+
+angular.module('noteApp')
+    .constant("baseURL", "/")
+    .service('noteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+        this.getNotes = function () {
+            return $resource(baseURL + "notes/:id", null, {'update': {method: 'PUT'}});
+        };
+    }]);
