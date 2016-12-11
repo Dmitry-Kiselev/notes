136298841979a0f0200b81945b430414e659ab06
Dmitry Kiselev <kdem27@gmail.com>
2016-12-11 13:16:34 +0200

    register remained models

--- note/admin.py
+++ note/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Note, Label, Category
 
+from .models import Note, Label, Category, Categorization, Labelling
 
 # Register your models here.
 admin.site.register(Note)
 admin.site.register(Label)
 admin.site.register(Category)
+admin.site.register(Labelling)
+admin.site.register(Categorization)
