9278a086eba624e1ac4f02640b0bd8bac777cdf3
Dmitry Kiselev <kdem27@gmail.com>
2016-12-17 20:16:27 +0200

    fix: update notes properly

--- note/static/js/NoteController.js
+++ note/static/js/NoteController.js
@@ -5,62 +5,82 @@
 
 angular.module('noteApp')
 
-    .controller('NoteController', function ($scope, $resource, noteFactory) {
+.controller('NoteController', function($scope, $resource, noteFactory) {
 
-        var DEFAULT_COLOR = "blue-grey darken-1";
+    var DEFAULT_COLOR = "blue-grey darken-1";
 
-        // create a blank object to handle form data.
-        $scope.userdata = {};
-        $scope.notes = [];
-        $scope.curNote = {};
+    // create a blank object to handle form data.
+    $scope.userdata = {};
+    $scope.notes = [];
+    $scope.curNote = {};
 
 
-        angular.element(document).ready(function () {
-            // Initialize collapse button
-            $("#sidenav-btn").sideNav();
-            // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-            $('#noteModal').modal();
+    angular.element(document).ready(function() {
+        // Initialize collapse button
+        $("#sidenav-btn").sideNav();
+        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+        $('#noteModal').modal();
+        $('select').material_select();
+        $scope.notes = noteFactory.notesManager().query(
+            function() {
+                $scope.showNotes = true;
+            }
+        );
+        $scope.labels = noteFactory.labelsManager().query(
+            function() {
+                $scope.showLabels = true;
+
+            }
+        );
+        $scope.categories = noteFactory.categoriesManager().query(
+            function() {
+                $scope.showCategories = true;
+            }
+        );
+
+        $scope.activateSelect = function() {
             $('select').material_select();
-            $scope.notes = noteFactory.notesManager().query(
-                function () {
-                    $scope.showNotes = true;
-                }
-            );
-            $scope.labels = noteFactory.labelsManager().query(
-                function () {
-                    $scope.showLabels = true;
+        };
 
-                }
-            );
-            $scope.categories = noteFactory.categoriesManager().query(
-                function () {
-                    $scope.showCategories = true;
-                }
-            );
-
-            $scope.activateSelect = function () {
-                $('select').material_select();
-            };
-
-            $scope.sendNote = function () {
-                if ($scope.curNote.id) {
-                    noteFactory.notesManager().update({id: $scope.curNote.id}, $scope.curNote);
-                    $scope.notes[$scope.curNote.id] = $scope.curNote;
-                    $scope.curNote = {}
-                }
-                else {
-                    noteFactory.notesManager().save($scope.curNote);
-                    $scope.curNote.color = DEFAULT_COLOR;
-                    $scope.notes.push($scope.curNote);
-                    $scope.curNote = {}
-                }
+        $scope.sendNote = function() {
+            if ($scope.curNote.id) {
+                noteFactory.notesManager().update({
+                    id: $scope.curNote.id
+                }, $scope.curNote);
+                var index = $scope.notes.findIndex(x => x.id == $scope.curNote.id);
+                $scope.notes[index] = $scope.curNote;
                 $scope.curNote = {};
-            };
+            } else {
+                noteFactory.notesManager().save($scope.curNote);
+                $scope.curNote.color = DEFAULT_COLOR;
+                $scope.notes.push($scope.curNote);
+                $scope.curNote = {}
+            }
+            $scope.curNote = {};
+        };
 
-            $scope.deleteNote = function (id) {
-                noteFactory.notesManager().delete({id: id});
-                var index = $scope.notes.findIndex(x = > x.id == id);
-                $scope.notes.pop(index);
-            };
-        });
+        $scope.deleteNote = function(id) {
+            noteFactory.notesManager().delete({
+                id: id
+            });
+            var index = $scope.notes.findIndex(x => x.id == id);
+            $scope.notes.pop(index);
+        };
+
+
+        $scope.editNote = function(id) {
+            var index = $scope.notes.findIndex(x => x.id == id);
+            $scope.curNote = $scope.notes[index];
+            $('#noteModal').modal('open');
+        };
+
+        $scope.checkSelected = function() {
+            try {
+                $('#label_select').val($scope.curNote.labels).trigger('update');
+                $('#label_select').material_select();
+            } catch (err) {
+                // do nothing
+            }
+        }
     });
+});
