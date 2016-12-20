f0ad160996e12333eceb3bc7e12e23a618d5f92e
Dmitry Kiselev <kdem27@gmail.com>
2016-12-20 19:33:05 +0200

    register new and delete old models

--- note/admin.py
+++ note/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Note, Label, Category, Categorization, Labelling
+from .models import Note, Label, Category, Image, File
 
 # Register your models here.
 admin.site.register(Note)
 admin.site.register(Label)
 admin.site.register(Category)
-admin.site.register(Labelling)
-admin.site.register(Categorization)
+admin.site.register(Image)
+admin.site.register(File)
