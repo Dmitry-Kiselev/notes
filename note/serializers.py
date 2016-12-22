097ad059ea0186d67d68510d508a66addbdaf5a6
Dmitry Kiselev <kdem27@gmail.com>
2016-12-22 20:36:28 +0200

    handle multiple images upload [1/4]

--- note/serializers.py
+++ note/serializers.py
@@ -3,56 +3,6 @@ from rest_framework import serializers
 from .models import Note, Label, Category, Image, File
 
 
-class Base64ImageField(serializers.ImageField):
-    """
-    A Django REST framework field for handling image-uploads through raw post data.
-    It uses base64 for encoding and decoding the contents of the file.
-
-    Heavily based on
-    https://github.com/tomchristie/django-rest-framework/pull/1268
-
-    Updated for Django REST framework 3.
-    """
-
-    def to_internal_value(self, data):
-        from django.core.files.base import ContentFile
-        import base64
-        import six
-        import uuid
-
-        # Check if this is a base64 string
-        if isinstance(data, six.string_types):
-            # Check if the base64 string is in the "data:" format
-            if 'data:' in data and ';base64,' in data:
-                # Break out the header from the base64 content
-                header, data = data.split(';base64,')
-
-            # Try to decode the file. Return validation error if it fails.
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                self.fail('invalid_image')
-
-            # Generate file name:
-            file_name = str(uuid.uuid4())[:12]  # 12 characters are more than enough.
-            # Get the file name extension:
-            file_extension = self.get_file_extension(file_name, decoded_file)
-
-            complete_file_name = "%s.%s" % (file_name, file_extension,)
-
-            data = ContentFile(decoded_file, name=complete_file_name)
-
-        return super(Base64ImageField, self).to_internal_value(data)
-
-    def get_file_extension(self, file_name, decoded_file):
-        import imghdr
-
-        extension = imghdr.what(file_name, decoded_file)
-        extension = "jpg" if extension == "jpeg" else extension
-
-        return extension
-
-
 class LabelSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.id')
 
@@ -70,8 +20,8 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ImageSerializer(serializers.ModelSerializer):
-    note = serializers.ReadOnlyField(source='note.id')
-    image = Base64ImageField(max_length=None, use_url=True, )
+    owner = serializers.ReadOnlyField(source='owner.id')
+    image = serializers.ImageField(max_length=None, use_url=True, )
 
     class Meta:
         model = Image
@@ -90,15 +40,26 @@ class NoteSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         labels = validated_data.pop('labels')
+        images = self.initial_data.get('images')
         note = Note.objects.create(**validated_data)
         for label in labels:
             note.labels.add(Label.objects.get(name=label["name"]))
+        if images:
+            for img in images:
+                image = Image.objects.get(pk=img['id'])
+                note.images.add(image)
         return note
 
     def update(self, instance, validated_data):
         if instance:
             labels = validated_data.pop('labels')
+            images = self.initial_data.get('images')
             instance.labels.clear()
+            instance.images.clear()
             for label in labels:
                 instance.labels.add(Label.objects.get(name=label["name"]))
+            if images:
+                for img in images:
+                    image = Image.objects.get(pk=img['id'])
+                    instance.images.add(image)
             return instance
