6e11df0dddae4b44a41004f32496fa279450a548
Dmitry Kiselev <kdem27@gmail.com>
2016-12-22 20:56:19 +0200

    add FileSerializer

--- note/serializers.py
+++ note/serializers.py
@@ -28,11 +28,21 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class FileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.id')
+    file = serializers.FileField(max_length=None, use_url=True, )
+
+    class Meta:
+        model = File
+        fields = '__all__'
+
+
 class NoteSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.id')
     labels = LabelSerializer(many=True)
     categories = CategorySerializer(many=True, read_only=True)
     images = ImageSerializer(many=True, read_only=True)
+    files = FileSerializer(many=True, read_only=True)
 
     class Meta:
         model = Note
@@ -41,6 +51,7 @@ class NoteSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         labels = validated_data.pop('labels')
         images = self.initial_data.get('images')
+        files = self.initial_data.get('files')
         note = Note.objects.create(**validated_data)
         for label in labels:
             note.labels.add(Label.objects.get(name=label["name"]))
@@ -48,18 +59,28 @@ class NoteSerializer(serializers.ModelSerializer):
             for img in images:
                 image = Image.objects.get(pk=img['id'])
                 note.images.add(image)
+        if files:
+            for f in files:
+                file = File.objects.get(pk=f['id'])
+                note.images.add(file)
         return note
 
     def update(self, instance, validated_data):
         if instance:
             labels = validated_data.pop('labels')
             images = self.initial_data.get('images')
+            files = self.initial_data.get('files')
             instance.labels.clear()
             instance.images.clear()
+            instance.files.clear()
             for label in labels:
                 instance.labels.add(Label.objects.get(name=label["name"]))
             if images:
                 for img in images:
                     image = Image.objects.get(pk=img['id'])
                     instance.images.add(image)
+            if files:
+                for f in files:
+                    file = File.objects.get(pk=f['id'])
+                    instance.files.add(file)
             return instance
