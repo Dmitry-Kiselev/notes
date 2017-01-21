107bdbe0a8d30d892cb7ace6b72cca207ce48681
Dmitry Kiselev <kdem27@gmail.com>
2017-01-21 16:08:12 +0200

    set up email backend

--- notes/settings.py
+++ notes/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your email account'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Note app'
