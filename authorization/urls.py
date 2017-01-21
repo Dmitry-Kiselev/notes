bb030d8f58ce412caf4fb9d9d33d8fc419a1999d
Dmitry Kiselev <kdem27@gmail.com>
2017-01-21 16:04:58 +0200

    [Forgot Password Functionality]: set urls

--- authorization/urls.py
+++ authorization/urls.py
@@ -14,10 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from .views import auth
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import logout
 
+from .views import auth
+
 urlpatterns = [
+    url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'authorization/password_reset_form.html'},
+        name='password_reset'),
+    url(r'^password_reset/done/$',
+        auth_views.password_reset_done, {'template_name': 'authorization/password_reset_done.html'},
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, {'template_name': 'authorization/password_reset_confirm.html'},
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete,
+        {'template_name': 'authorization/password_reset_complete.html'}, name='password_reset_complete'),
     url(r'logout', logout, {"next_page": "/"}, name='logout'),
     url(r'$', auth, name='auth'),
 ]
